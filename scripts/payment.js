5b82c4890ed7c4f207485f81561c605efecac6e4
TejasBharaswadkar <101712924+TejasBharaswadkar@users.noreply.github.com>
2022-07-31 16:33:18 +0530

    API Key Change

--- scripts/payment.js
+++ scripts/payment.js
@@ -1,7 +1,7 @@
 let urlParams = new URLSearchParams(window.location.search);
 const API_URL = 'https://travel-advisor.p.rapidapi.com/';
 const tripAdvisorHost = 'travel-advisor.p.rapidapi.com';
-const tripAdvisorKey = '289f16ea54msh62210c94ea30784p1721fbjsn45935df11aa5';
+const tripAdvisorKey = '376546c246mshf5818e5483096dap1ec6';
 
 /* Fetch the API data for hotel details, and price calculation, and finally create all the text dynamically */
 let fetchAPI = () => {
